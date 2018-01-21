7cdf1e7b525dc096a4933c42e1fd99855ddf161a
James Sarkar <James.Sarkar@sap.com>
2018-01-21 17:58:58 -0500

    Added respawn points and logic to respawn player

--- Assets/Scripts/Player Scripts/PlayerHealth.cs
+++ Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -33,9 +33,35 @@ public class PlayerHealth : MonoBehaviour {
 		health -= damage;
 
 		if (health <= 0) {
-
+			StartCoroutine (Die ());
 		} else {
 			SendMessage ("Slam", transform.TransformDirection (slamDir));
+		}
+	}
+
+	IEnumerator Die () {
+		AudioSource.PlayClipAtPoint (deathSound, transform.position);
+
+		lives--;
+
+		if (lives < 0) {
+			// TODO: End the game
+		} else {
+			fellDown = false;
+
+			health = maxHealth;
+
+			Vector3 respawnPos = GameObject.Find ("Respawn0").transform.position;
+
+			Camera.main.transform.position = respawnPos - (transform.forward * 4) + Vector3.up;
+
+			SendMessage ("HidePlayer");
+
+			transform.position = respawnPos;
+
+			yield return new WaitForSeconds (1.6f);
+
+			SendMessage ("ShowPlayer");
 		}
 	}
 
